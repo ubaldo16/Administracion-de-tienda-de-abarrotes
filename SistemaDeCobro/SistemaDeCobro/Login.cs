23b657314e5311cfe620610592823d5e530fb941
HectorUA <hector-azua@hotmail.com>
2019-05-17 16:23:41 -0500

    Add files via upload

---
+++ SistemaDeCobro/SistemaDeCobro/Login.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaDeCobro
+{
+	public partial class Login : Form
+	{
+		public Login()
+		{
+			InitializeComponent();
+		}
+
+		private void buttonLog_Click(object sender, EventArgs e) //Login
+		{           
+            string cmdText = "select Count(*) from Empleado where RFC=? and [Contraseña]=?";
+            using (OleDbConnection con = new OleDbConnection(Properties.Settings.Default.ConexionDB))
+            using (OleDbCommand cmd = new OleDbCommand(cmdText, con))
+            {
+                con.Open();
+                cmd.Parameters.AddWithValue("@p1", textboxUser.Text);
+                cmd.Parameters.AddWithValue("@p2", textBoxPass.Text);  // <- is this a variable or a textbox?
+                int result = (int)cmd.ExecuteScalar();
+                if (result > 0)
+                {
+                    MessageBox.Show("Login Successful");
+                    string comprobacion = "SELECT Privilegio FROM Empleado WHERE RFC = @rfc";
+                    OleDbCommand comando = new OleDbCommand(comprobacion, con);
+                    comando.Parameters.AddWithValue("@rfc", textboxUser.Text);
+
+                    OleDbDataReader lector = comando.ExecuteReader();
+
+                    if (lector.Read())
+                    {
+                        Menu m = new Menu(Int32.Parse(lector["Privilegio"].ToString()));//checar logeo si es usuario o administrador
+                        m.Show();
+                        this.Visible = false;
+                    }
+                    
+
+                }
+                else
+                    MessageBox.Show("Invalid Credentials, Please Re-Enter");
+
+                con.Close();
+            }
+        
+		}
+	}
+}
---
+++ SistemaDeCobro/SistemaDeCobro/Usuarios.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaDeCobro
+{
+	public partial class FormularioUsuarios : Form
+	{
+		public FormularioUsuarios()
+		{
+			InitializeComponent();
+		}
+
+		private void butSearch_Click(object sender, EventArgs e)
+		{
+			UsuariosL Ul = new UsuariosL();
+			Ul.Show();
+		}
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void WorkedRegister()
+        {
+            try
+            {
+                OleDbCommand cmd = new OleDbCommand();
+                
+                OleDbConnection conexion = new OleDbConnection(Properties.Settings.Default.ConexionDB);
+                cmd.Connection = conexion;
+                conexion.Open();
+                string insertar = "INSERT INTO Empleado(RFC,Nombre,[Apellido Materno],[Apellido Paterno],Numero_Telefonico,Calle,Colonia,Numero_Exterior,CP,Contraseña,Privilegio) VALUES ( '"+ textRFC.Text+ "', '"+textNombre.Text+"' ,'"+textApellidoM.Text+"', '"+textApellidoP.Text+"', '"+Int32.Parse(textTel.Text)+"', '"+textCalle.Text+"','"+textCol.Text+"','"+Int32.Parse(textNumExt.Text)+"','"+Int32.Parse(textCP.Text)+"','"+Pass.Text+"', '"+Privilegio.SelectedIndex+"')";
+                cmd.CommandText = @insertar;
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Registro guardado");
+                conexion.Close();
+
+            }
+            catch(DBConcurrencyException ex)
+            {
+                MessageBox.Show("Error de concurrencia:\n" + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void ButAdd_Click(object sender, EventArgs e)
+        {
+            WorkedRegister();
+        }
+    }
+}
